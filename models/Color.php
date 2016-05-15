b04d546e508b6bc6971523f3031c8c96640d8d2d
Mykhailo Shatilov <proger.mixa@gmail.com>
2016-05-14 20:10:02 -0400

    add default model class

---
+++ models/Color.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace iproger\colorlist\models;
+
+/**
+ * This is the model class for table "color".
+ *
+ * @property integer $id
+ * @property string $name
+ */
+class Color extends \yii\db\ActiveRecord
+{
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'color';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+        [['name'], 'required'],
+        [['name'], 'string', 'max' => 255],
+        [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+        'id'   => 'ID',
+        'name' => 'Name',
+        ];
+    }
+
+}

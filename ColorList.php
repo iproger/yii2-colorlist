60be1005d8b6c6b71bd1779febe68bd0b58f0a38
Mykhailo Shatilov <proger.mixa@gmail.com>
2016-05-14 20:22:18 -0400

    change getColors() and create getModel() in ColorList

--- ColorList.php
+++ ColorList.php
@@ -3,13 +3,25 @@
 namespace iproger\colorlist;
 
 use yii\helpers\ArrayHelper;
+use iproger\colorlist\models\Color;
 
 class ColorList extends widgets\ColorList\AbstractColorList
 {
 
     public function getColors()
     {
-        return ArrayHelper::map(Color::find()->orderBy(['id' => SORT_ASC])->all(), 'id', 'name');
+        $model = $this->getModel();
+
+        $colors = $model::find()
+            ->orderBy(['id' => SORT_ASC])
+            ->all();
+
+        return ArrayHelper::map($colors, 'id', 'name');
+    }
+
+    public function getModel()
+    {
+        return Color;
     }
 
 }

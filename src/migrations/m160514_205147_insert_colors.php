695063602b00c7b88ae7fba1a47b7b4bbf9fe714
Mykhailo Shatilov <proger.mixa@gmail.com>
2016-09-17 22:17:24 -0400

    add insert data (fix #1)

--- src/migrations/m160514_205147_insert_colors.php
+++ src/migrations/m160514_205147_insert_colors.php
@@ -11,17 +11,63 @@ use yii\db\Migration;
 class m160514_205147_insert_colors extends Migration
 {
 
-    private $tableName = 'color';
+    private $tableName = '{{%color}}';
 
     /**
      * @inheritdoc
      */
     public function up()
     {
-        $this->insert($this->tableName, ['name' => 'Red']);
-        $this->insert($this->tableName, ['name' => 'White']);
-        $this->insert($this->tableName, ['name' => 'Blue']);
-        $this->insert($this->tableName, ['name' => 'Black']);
+        $columns = ['name'];
+        $rows    = [
+            [
+                'Red',
+            ],
+            [
+                'White',
+            ],
+            [
+                'Blue',
+            ],
+            [
+                'Black',
+            ],
+            [
+                'Mixed',
+            ],
+            [
+                'Green',
+            ],
+            [
+                'Pink',
+            ],
+            [
+                'Brown',
+            ],
+            [
+                'Coffee',
+            ],
+            [
+                'Gold',
+            ],
+            [
+                'Olive',
+            ],
+            [
+                'Rose',
+            ],
+            [
+                'Beige',
+            ],
+            [
+                'Cyan',
+            ],
+            [
+                'Aqua',
+            ],
+        ];
+
+        $this->batchInsert($this->tableName, $columns, $rows);
     }
 
     /**
